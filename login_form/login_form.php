7752c0a0a2f1439df51db1f5c7db26a39f9aca85
programer-sheuly <125537194+programer-sheuly@users.noreply.github.com>
2023-05-29 11:55:17 +0600

    Add files via upload

---
+++ login_form/login_form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login_Form</title>
+</head>
+<body>
+    <div class="container">
+        <div class="login">
+            <h1>Hello Again!</h1>
+                <form action="" method="post">
+                    <label for=""></label>
+                </form>
+
+        </div>
+    </div>
+</body>
+</html>
